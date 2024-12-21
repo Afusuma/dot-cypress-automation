6ec12e8f0aaafcbc04aa354a8a10bd3fdefe4fde
afusuma <amanda_fusuma@hotmail.com>
2024-12-20 23:25:08 -0300

    login implementado com configuracoes de timeout + intercept

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://www.saucedemo.com",
+    chromeWebSecurity: false, 
+    defaultCommandTimeout: 10000, 
+    pageLoadTimeout: 120000, 
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
-});
+})
